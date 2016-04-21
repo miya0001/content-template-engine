fa9968c91e311f27b55478dd60c445bb0f569730
miya0001 <miya@wpist.me>
2016-04-21 15:41:15 +0900

    update v0.9.4

--- content-template-engine.php
+++ content-template-engine.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Content Template Engine
-Version: 0.9.3
+Version: 0.9.4
 Description: Enables Twig template engine in the WordPress contents.
 Author: Takayuki Miyauchi
 Author URI: https://github.com/miya0001/
